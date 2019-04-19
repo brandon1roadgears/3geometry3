24192d9c88f5aefc2bba0d4afa1c6f4c7933e6a4
daniel <danil_timchenko_1997@mail.ru>
2019-04-19 20:10:40 +0700

    Print (f) in main

--- src/main.cpp
+++ src/main.cpp
@@ -12,5 +12,40 @@ int main()
 		cin >> x2 >> y2 >> r2;
 	perim2 = getperim(r2);
 	s2 = getarea(r2);
+	cout << endl;
+	d = r1 + r2;
+	if ((r1<r2 || r2<r1) && x1 == x2 && y1 == y2) {
+		flag = 0;
+	}
+	else if (r1 == r2 && x1 == x2 && y1 == y2) {
+		flag = 1;
+	}
+	if (d + r1<r2) {
+		flag = 0;
+	}
+	else if (((d + r1) == r2) || d + r1>r2) {
+		flag = 1;
+	}
+	if (d>r1 + r2) {
+		flag = 1;
+	}
+	else if ((d == r1 + r2) || d<(r1 + r2)) {
+		flag = 1;
+	
+	cout << "1" << endl;
+	printf("circle(%d %d , %.1f)\n", x1, y1, r1);
+	printf("perimetr = %.3f\n", perim1);
+	printf("area = %.3f\n", S1);
+	if (flag == 1) {
+		printf("intersects: \n circle \n ");
+	}
+	cout << "2" << endl;
+	printf("circle(%d %d , %.1f)\n", x2, y2, r2);
+	printf("perimetr = %.3f\n", perim2);
+	printf("area = %.3f\n", S2);
+	if (flag == 1) {
+		printf("intersects: \n circle \n ");
+	}
+	system("PAUSE");
 	return 0;
 }
