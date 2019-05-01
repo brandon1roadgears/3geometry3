b7314b3e9ceb20e0e27c8fcd59632372e1e2868f
daniel <danil_timchenko_1997@mail.ru>
2019-05-01 13:12:36 +0700

    Rewrite test in getperim with tochnost

--- test/TESTgetperim.cpp
+++ test/TESTgetperim.cpp
@@ -7,8 +7,8 @@ float getperim(float r)
 }
 
 TEST_CASE("GETPERIM", "[PERIM]") {
-  REQUIRE(getperim(3.3) == 20.73451f);
-  REQUIRE(getperim(6.11) == 38.3903f);
+  REQUIRE(getperim(3.3) == 20.7345123291015625f);
+  REQUIRE(getperim(6.11) == 38.390262603759765625f);
   REQUIRE(getperim(4.21)  == 26.4522f);
   REQUIRE(getperim(8.98) == 56.423f);
   REQUIRE(getperim(7.01) == 44.0451f);
