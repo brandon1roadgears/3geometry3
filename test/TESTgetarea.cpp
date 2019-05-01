c63b149c279d282d77fa8384fe41769fd198c31e
daniel <danil_timchenko_1997@mail.ru>
2019-05-01 12:40:08 +0700

    retest with double

--- test/TESTgetarea.cpp
+++ test/TESTgetarea.cpp
@@ -9,9 +9,5 @@ float getarea(float r)
 TEST_CASE("GETAREA", "[AREA]") {
   REQUIRE(getarea(2.2) == 15.20531f);
   REQUIRE(getarea(6.11) == 117.28226f);
-  REQUIRE(getarea(4.21) == 55.6819f);
-  REQUIRE(getarea(8.98) == 253.339f);
-  REQUIRE(getarea(7.01) == 154.378f);
-  REQUIRE(getarea(10.10) == 320.474f);
   cout << "Test getarea is done" << endl;
 }
